bc971972d4389a28f241f4a3d54a8742a0c62fb5
Joan Romero <romerojoan1999@gmail.com>
2023-11-08 10:40:14 -0400

    add register page

---
+++ src/app/register/page.js
@@ -0,0 +1,23 @@
+import RegisterForm from '@/components/RegisterForm';
+import Link from 'next/link';
+
+function Register() {
+	return (
+		<div className="h-screen flex items-center justify-center">
+			<div className="w-full lg:max-w-[600px]">
+				<h1 className="font-bold text-4xl text-red-500 sm:text-2xl mb-8">
+					Crear cuenta
+				</h1>
+				<RegisterForm />
+				<div className="flex justify-center mt-6 text-sm">
+					<span className="mr-1 text-gray-500">¿Ya tienes una cuenta?</span>
+					<Link href="/login" className="text-red-500 hover:underline">
+						Inicia sesión
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Register;
